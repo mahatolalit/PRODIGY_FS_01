1f9a0d3689ff1e0b81eb2f210952423b62ed88e3
mahatolalit <lalitmahato20051024@gmail.com>
2025-06-29 19:03:26 +0530

    Production Setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,15 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
+import path from "path";
+import { fileURLToPath } from 'url';
 
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); // Enable CORS with credentials
 
@@ -15,6 +20,14 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser()); // Middleware to parse cookies
 
 app.use("/api/auth", authRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  });
+}
 
 app.listen(PORT, () => {
     connectDB();
